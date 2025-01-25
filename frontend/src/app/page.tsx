3b999aeba0fdd7d643ba32b2d1badf339859dcc6
ykchir <yecine.kchir@gmail.com>
2025-01-25 17:22:17 +0100

    feat(frontend-paginated-vehicle-list): add pagination

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,32 @@
 import VehicleCard from "@/components/vehicles/VehicleCard";
+import Pagination from "@/components/ui/Pagination";
 import { fetchVehicles } from "@/services/vehicleService";
 import { Vehicle } from "shared-types";
 
-export default async function Home() {
-  const { vehicles }: { vehicles: Vehicle[] } = await fetchVehicles();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page?: string } | Promise<{ page?: string }>;
+}) {
+  const resolvedSearchParams = await searchParams;
+  const page = parseInt(resolvedSearchParams?.page || "1", 10);
+
+  const limit = 6;
+
+  const { vehicles, total }: { vehicles: Vehicle[]; total: number } =
+    await fetchVehicles(page, limit);
+
+  const totalPages = Math.ceil(total / limit);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {vehicles.map((vehicle) => (
-        <VehicleCard key={vehicle.id} vehicle={vehicle} />
-      ))}
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {vehicles.map((vehicle) => (
+          <VehicleCard key={vehicle.id} vehicle={vehicle} />
+        ))}
+      </div>
+
+      <Pagination currentPage={page} totalPages={totalPages} />
     </div>
   );
 }
