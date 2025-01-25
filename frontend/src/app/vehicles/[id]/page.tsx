fcae2cfcaada6ed8f523727c1349586a519f341e
ykchir <yecine.kchir@gmail.com>
2025-01-25 21:20:43 +0100

    fix(frontend-workflow): fix linter errors

--- frontend/src/app/vehicles/[id]/page.tsx
+++ frontend/src/app/vehicles/[id]/page.tsx
@@ -5,7 +5,7 @@ import VehicleInfo from "@/components/vehicles/VehicleInfo";
 export default async function VehicleDetails({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
   const vehicle = await fetchVehicleById(id);
